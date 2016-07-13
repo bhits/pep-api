be3f21c9d4506cbcece71ad1d5258596f2863206
JiahaoLi <Jiahao.Li@feisystems.com>
2016-07-13 00:03:18 -0400

    Remove unused styling code for redact
    #9521 - As a provider, I can request a patient's data from an HIEOS, based on Patient’s 42 CFR Part 2 compliant consent (Back-end) [C2, C3, C5]

--- pep/src/main/java/gov/samhsa/c2s/pep/service/PolicyEnforcementPointImpl.java
+++ pep/src/main/java/gov/samhsa/c2s/pep/service/PolicyEnforcementPointImpl.java
@@ -1,6 +1,5 @@
 package gov.samhsa.c2s.pep.service;
 
-import com.netflix.discovery.converters.Auto;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
 import feign.FeignException;
 import gov.samhsa.c2s.pep.infrastructure.ContextHandlerService;
@@ -15,9 +14,7 @@ import gov.samhsa.c2s.pep.service.exception.InternalServerErrorException;
 import gov.samhsa.c2s.pep.service.exception.ProviderNotFoundException;
 import gov.samhsa.mhc.common.document.accessor.DocumentAccessor;
 import gov.samhsa.mhc.common.document.accessor.DocumentAccessorException;
-import gov.samhsa.mhc.common.document.accessor.DocumentAccessorImpl;
 import gov.samhsa.mhc.common.document.converter.DocumentXmlConverter;
-import gov.samhsa.mhc.common.document.converter.DocumentXmlConverterImpl;
 import gov.samhsa.mhc.common.document.transformer.XmlTransformer;
 import gov.samhsa.mhc.common.param.Params;
 import lombok.extern.slf4j.Slf4j;
@@ -26,28 +23,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
-import javax.annotation.PostConstruct;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.URIResolver;
-import java.io.*;
-import java.lang.reflect.Array;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -142,28 +124,28 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
     @Override
     public SegmentedDocumentsResponseDto getCCDDocuments(String username, DocumentRequestDto documentRequestDto) {
         final String recipientNpi = Optional.ofNullable(username)
-                                            .map(providerNpiLookupService.getUsers()::get)
-                                            .orElseThrow(ProviderNotFoundException::new);
+                .map(providerNpiLookupService.getUsers()::get)
+                .orElseThrow(ProviderNotFoundException::new);
 
         String parameters = composeIExHubParameter(documentRequestDto.getMrn(), documentRequestDto.getDomain());
         HttpHeaders headers = new HttpHeaders();
-        headers.set(iexhubParameterKey,parameters );
+        headers.set(iexhubParameterKey, parameters);
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
         // Calling IExHub to get ccd document
-        ResponseEntity<DocumentsResponseDto> response  = restTemplate.exchange(iexhubUrl, HttpMethod.GET, entity, DocumentsResponseDto.class);
+        ResponseEntity<DocumentsResponseDto> response = restTemplate.exchange(iexhubUrl, HttpMethod.GET, entity, DocumentsResponseDto.class);
 
-        if(!response.getStatusCode().equals(HttpStatus.OK)){
+        if (!response.getStatusCode().equals(HttpStatus.OK)) {
             throw new DocumentNotFoundException("Cannot get CCD document from IExhub.");
         }
 
         Optional<String> intermediateNPI = Optional.empty();
         DocumentsResponseDto documentsResponseDto = response.getBody();
 
-        if(documentsResponseDto.getDocuments().size() > 0){
+        if (documentsResponseDto.getDocuments().size() > 0) {
             PatientDocument patientDocument = documentsResponseDto.getDocuments().get(0);
             String documentStr = patientDocument.getDocument();
-            intermediateNPI = getIntermediateNPI(recipientNpi,documentStr );
+            intermediateNPI = getIntermediateNPI(recipientNpi, documentStr);
             XacmlRequestDto xacmlRequestDto = createXacmlRequestDto(recipientNpi, intermediateNPI.get(), documentRequestDto);
             XacmlResponseDto xacmlResponseDto = contextHandler.enforcePolicy(xacmlRequestDto);
 
@@ -179,7 +161,7 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
             String segmentedDocumentStr = new String(dssResponse.getSegmentedDocument());
             final Document segmentedClinicalDocumentDoc = documentXmlConverter.loadDocument(segmentedDocumentStr);
             // xslt transformation
-            final String xslUrl = Thread.currentThread().getContextClassLoader().getResource("CCDA_flag_redact.xsl").toString();
+            final String xslUrl = Thread.currentThread().getContextClassLoader().getResource("CCDA.xsl").toString();
 
             final String output = xmlTransformer.transform(segmentedClinicalDocumentDoc, xslUrl, Optional.<Params>empty(), Optional.<URIResolver>empty());
             patientDocument.setDocument(output);
@@ -190,10 +172,10 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
         return new SegmentedDocumentsResponseDto();
     }
 
-    private SegmentedDocumentsResponseDto toSegmentedDocumentResponse(DocumentsResponseDto documentsResponseDto){
+    private SegmentedDocumentsResponseDto toSegmentedDocumentResponse(DocumentsResponseDto documentsResponseDto) {
         ArrayList<SegmentedPatientDocument> segmentedPatientDocuments = new ArrayList<SegmentedPatientDocument>();
-        for(PatientDocument p : documentsResponseDto.getDocuments()){
-            SegmentedPatientDocument segmentedPatientDocument =  new SegmentedPatientDocument(p.getName(), p.getDocument().getBytes(StandardCharsets.UTF_8));
+        for (PatientDocument p : documentsResponseDto.getDocuments()) {
+            SegmentedPatientDocument segmentedPatientDocument = new SegmentedPatientDocument(p.getName(), p.getDocument().getBytes(StandardCharsets.UTF_8));
             segmentedPatientDocuments.add(segmentedPatientDocument);
         }
 
@@ -202,11 +184,11 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
         return segmentedDocumentsResponseDto;
     }
 
-    private XacmlRequestDto createXacmlRequestDto(String recipientNpi, String intermediateNPI, DocumentRequestDto documentRequestDto){
+    private XacmlRequestDto createXacmlRequestDto(String recipientNpi, String intermediateNPI, DocumentRequestDto documentRequestDto) {
         XacmlRequestDto xacmlRequestDto = new XacmlRequestDto();
         xacmlRequestDto.setIntermediaryNpi(intermediateNPI);
 
-        PatientIdDto patientIdDto =  new PatientIdDto();
+        PatientIdDto patientIdDto = new PatientIdDto();
         patientIdDto.setExtension(documentRequestDto.getMrn());
         patientIdDto.setRoot(documentRequestDto.getDomain());
 
@@ -218,19 +200,20 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
 
         return xacmlRequestDto;
     }
-    private String composeIExHubParameter(String mrn, String domain){
-        if(mrn!=null && domain!=null && iexhubParameterSuffix != null){
+
+    private String composeIExHubParameter(String mrn, String domain) {
+        if (mrn != null && domain != null && iexhubParameterSuffix != null) {
             String patientId = mrn + "^^^&" + domain + "&ISO";
-            return  iexhubParameterSuffix.replace("PATIENT_ID_VALUE", "'" + patientId + "'");
+            return iexhubParameterSuffix.replace("PATIENT_ID_VALUE", "'" + patientId + "'");
         }
         return null;
     }
 
-    private String getRootFromMRN(String mrn){
+    private String getRootFromMRN(String mrn) {
         return mrn.split("&")[1];
     }
 
-    private String getExtensionFromMRN(String mrn){
+    private String getExtensionFromMRN(String mrn) {
         return mrn.split("&")[1];
     }
 
@@ -240,16 +223,16 @@ public class PolicyEnforcementPointImpl implements PolicyEnforcementPoint {
 
         try {
             Optional<Node> optionalNode = documentAccessor.getNode(ccdDocument, "/hl7:ClinicalDocument/hl7:author[1]/hl7:assignedAuthor[1]/hl7:id[@root='2.16.840.1.113883.4.6'][1]/@extension");
-            if(optionalNode.isPresent()){
+            if (optionalNode.isPresent()) {
                 Node node = optionalNode.get();
                 String value = node.getNodeValue();
                 return optionalNode.map(Node::getNodeValue);
-            }else{
+            } else {
                 log.error("Cannot find provider NPI in document.");
             }
 
-        }catch (DocumentAccessorException e){
-                log.error(e.getStackTrace().toString());
+        } catch (DocumentAccessorException e) {
+            log.error(e.getStackTrace().toString());
         }
         return Optional.empty();
     }
