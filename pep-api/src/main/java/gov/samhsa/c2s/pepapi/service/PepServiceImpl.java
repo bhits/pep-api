ab81bfd348611f3e9ea0efb4dfd5700e29883796
sadhch <sadhana.chandravanka@feisystems.com>
2017-06-19 11:51:46 -0400

    Add Error Handling Code
    #14009 - As a developer I will have implemented backend to support - Segmentation/Upload/download patient health record workflow

--- pep-api/src/main/java/gov/samhsa/c2s/pepapi/service/PepServiceImpl.java
+++ pep-api/src/main/java/gov/samhsa/c2s/pepapi/service/PepServiceImpl.java
@@ -1,11 +1,20 @@
 package gov.samhsa.c2s.pepapi.service;
 
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import feign.FeignException;
 import gov.samhsa.c2s.pepapi.infrastructure.PepClient;
 import gov.samhsa.c2s.pepapi.infrastructure.dto.AccessRequestDto;
 import gov.samhsa.c2s.pepapi.infrastructure.dto.AccessResponseDto;
+import gov.samhsa.c2s.pepapi.service.exception.NoDocumentFoundException;
+import gov.samhsa.c2s.pepapi.service.exception.PepClientInterfaceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class PepServiceImpl implements PepService {
 
     private final PepClient pepClient;
@@ -16,6 +25,33 @@ public class PepServiceImpl implements PepService {
 
     @Override
     public AccessResponseDto accessDocument(AccessRequestDto accessRequest) {
-        return pepClient.access(accessRequest);
+        AccessResponseDto accessResponseDto;
+        try {
+            accessResponseDto = pepClient.access(accessRequest);
+
+        } catch (HystrixRuntimeException hystrixErr) {
+            Throwable causedBy = hystrixErr.getCause();
+
+            if (!(causedBy instanceof FeignException)) {
+                log.error("Unexpected instance of HystrixRuntimeException has occurred", hystrixErr);
+                throw new PepClientInterfaceException("An unknown error occurred while attempting to communicate with" +
+                        " PEP service", hystrixErr);
+            }
+
+            int causedByStatus = ((FeignException) causedBy).status();
+
+            switch (causedByStatus) {
+                case 404:
+                    log.error("PEP client returned a 404 - NO Consent/Document Found REQUEST status" +
+                            " to PEP client", causedBy);
+                    throw new NoDocumentFoundException("Invalid document was passed to DSS client");
+                default:
+                    log.error("DSS client returned an unexpected instance of FeignException", causedBy);
+                    throw new PepClientInterfaceException("An unknown error occurred while attempting to communicate " +
+                            "with" +
+                            " PEP service");
+            }
+        }
+        return accessResponseDto;
     }
 }
